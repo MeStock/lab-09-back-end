e72e275ddd22b344c0ea61e99ff80d1b092fa9cc
mestock <MelissaStock44@gmail.com>
2019-04-20 13:27:09 -0700

    Organized code into appropriate sections for better readbility

--- server.js
+++ server.js
@@ -5,22 +5,20 @@
 
 require('dotenv').config();
 
-
-
-//------------------------GLOBAL CONSTANTS---------------------------------
+//-------------------GLOBAL CONSTANTS------------------------------------
 const PORT = process.env.PORT || 3000;
 const express = require('express');
 const cors = require('cors');
 const superagent = require('superagent');
 const pg = require('pg');
 
-//-------------------POSTGRES CLIENT-----------------------------------------
+//-------------------POSTGRES CLIENT-------------------------------------
 const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
 client.on('error', error => console.error(error))
 
 
-//-----------------------------SERVER DEFINITION-----------------------------
+//-------------------SERVER DEFINITION-----------------------------------
 const app = express();
 app.use(cors());
 
@@ -34,7 +32,8 @@ app.use('*', (request, response) => {
   response.send('Our server runs.');
 })
 
-//-----------------------------SQL COMMANDS----------------------------
+//---------------------SQL COMMANDS---------------------------------------
+//TODO: Find a way to condense this code and make more dry.
 const SQL_CMDS = {};
 SQL_CMDS.getLocation = 'SELECT * FROM locations WHERE search_query=$1'
 SQL_CMDS.insertLocation = 'INSERT INTO locations (search_query, formatted_query, latitude, longitude) VALUES ($1, $2, $3, $4) RETURNING *;'
@@ -51,11 +50,9 @@ SQL_CMDS.getRestaurants = 'SELECT * FROM restaurants WHERE location_id=$1;'
 SQL_CMDS.insertRestaurants = 'INSERT INTO restaurants (name, image_url, price, rating, url, created_at, location_id) VALUES ($1, $2, $3, $4, $5, $6, $7);'
 SQL_CMDS.deleteRestaurant = 'DELETE FROM restaurants WHERE location_id=$1;'
 
-
-
-
-
-//----------------CONSTRUCTOR FUNCTIONS-----------------------------------
+//------------------------------------------------------------
+//--------------------CONSTRUCTOR FUNCTIONS-------------------
+//------------------------------------------------------------
 
 function LocationData(search_query, formatted_query, latitude, longitude) {
   this.search_query = search_query;
@@ -69,7 +66,6 @@ function WeatherData(summary, time, location_id) {
 	this.time = time;
 	this.location_id = location_id;
 	this.created_at = Date.now();
-
 }
 
 function MovieData(title, overview, average_votes, total_votes, img_url, popularity, released_on, location_id){
@@ -93,9 +89,11 @@ function YelpData(name, image_url, price, rating, url, location_id){
 	this.created_at = Date.now();
 }
 
-//----------------------OTHER FUNCTIONS----------------------
+//------------------------------------------------------------
+//--------------------OTHER FUNCTIONS-------------------------
+//------------------------------------------------------------
 
-//----------------------CHECK DATABASE------------------------
+//-------------------CHECK LOCATIONS DATABASE-----------------
 function checkLocationDatabase(search_query, response) {
   return client.query(SQL_CMDS.getLocation, [search_query]).then(result => {
     if (result.rows.length) {
@@ -105,6 +103,7 @@ function checkLocationDatabase(search_query, response) {
     }
   });
 }
+//-------------------ALL OTHER DATABASES-----------------------
 function checkDatabase(location_id, response, SQL_get, SQL_delete, timeToRefresh) {
 		return client.query(SQL_get, [location_id]).then(result => {
 			if (result.rows.length){
@@ -118,38 +117,9 @@ function checkDatabase(location_id, response, SQL_get, SQL_delete, timeToRefresh
 			}
 		});
 	}
-
-	// function checkMovieDatabase(location_id, response){
-	// 	return client.query(SQL_CMDS.getRestaurants, [location_id]).then(result => {
-	// 		if (result.rows.length){
-	// 			//update every year
-	// 			if(Date.now() - result.rows[0].created_at > 31536000000){
-	// 				client.query(SQL_CMDS.deleteMovie, [location_id])
-	// 				return 'NOT IN DATABASE';
-	// 			}
-	// 			response.send(result.rows);
-	// 		}else {
-	// 			return 'NOT IN DATABASE';
-	// 		}
-	// 	});
-	// }
-
-	// function checkRestaurantDatabase(location_id, response){
-	// 	return client.query(SQL_CMDS.getRestaurants, [location_id]).then(result => {
-	// 		if (result.rows.length){
-	// 			//update every week
-	// 			if(Date.now() - result.rows[0].created_at > 604800000){
-	// 				client.query(SQL_CMDS.deleteRestaurant, [location_id])
-	// 				return 'NOT IN DATABASE';
-	// 			}
-	// 			response.send(result.rows);
-	// 		}else {
-	// 			return 'NOT IN DATABASE';
-	// 		}
-	// 	});
-	// }
-
-//-------------------GEOCODE----------------------------------
+//------------------------------------------------------------
+//----------------------GEOCODE-------------------------------
+//------------------------------------------------------------
 function searchLocationData(request, response) {
   //stores user input
   const search_query = request.query.data;
@@ -170,20 +140,22 @@ function searchLocationData(request, response) {
         const responseDataObject = new LocationData(search_query, formatted_query, latitude, longitude);
 
         client.query(SQL_CMDS.insertLocation, [responseDataObject.search_query, responseDataObject.formatted_query, responseDataObject.latitude, responseDataObject.longitude]).then(result => {
-					
-        	response.send(result.rows[0]);
+
+        response.send(result.rows[0]);
         }
         );
       })
     }
   });
 }
-
+//------------------------------------------------------------
 //-------------------DARKSKY----------------------------------
+//------------------------------------------------------------
 function searchWeatherData(request, response) {
 	const URL = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${request.query.data.latitude},${request.query.data.longitude}`;
 	let location_id = request.query.data.id;
 
+	//15000 = update every 15sec
 	checkDatabase(location_id, response, SQL_CMDS.getWeather, SQL_CMDS.deleteWeather, 15000).then(result => {
 		if(result === 'NOT IN DATABASE'){
 			superagent.get(URL).then(result => {
@@ -192,11 +164,11 @@ function searchWeatherData(request, response) {
 					const dailyWeather = dailyData.map((dailyDataObj) => {
 					let summary = dailyDataObj.summary;
 					let time = new Date(dailyDataObj.time * 1000).toString().slice(0, 15);
-		
+
 					const responseWeatherData = new WeatherData(summary, time, location_id);
-						
+
 					client.query(SQL_CMDS.insertWeather, [responseWeatherData.forecast, responseWeatherData.time, responseWeatherData.location_id, responseWeatherData.created_at]);
-		
+
 					return responseWeatherData;
 					});
 					response.send(dailyWeather);
@@ -205,11 +177,14 @@ function searchWeatherData(request, response) {
     }
   });
 }
-//-------------------MOVIES----------------------------------
+//------------------------------------------------------------
+//-------------------MOVIES-----------------------------------
+//------------------------------------------------------------
 function searchMovieData(request, response){
 	const URL = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${request.query.data.search_query}`;
 	let location_id = request.query.data.id;
 
+	//31536000000 = update every year
 	checkDatabase(location_id, response, SQL_CMDS.getMovies, SQL_CMDS.deleteMovie, 31536000000).then(result => {
 		if(result === 'NOT IN DATABASE'){
 			superagent.get(URL).then(result => {
@@ -222,41 +197,43 @@ function searchMovieData(request, response){
 					let img_url = movieObj.poster_path;
 					let popularity = movieObj.popularity;
 					let released_on = movieObj.release_date;
-					
+
 					const responseMovieObject = new MovieData(title, overview, average_votes, total_votes, img_url, popularity, released_on, location_id);
-		
+
 					client.query(SQL_CMDS.insertMovies, [responseMovieObject.title, responseMovieObject.overview, responseMovieObject.average_votes, responseMovieObject.total_votes, responseMovieObject.image_url, responseMovieObject.popularity, responseMovieObject.released_on, responseMovieObject.created_at, responseMovieObject.location_id]);
-		
+
 					return responseMovieObject;
 				});
 				response.send(topMovies);
-		
+
 			});
 		}
 	});
 }
-
-//---------------------YELP-------------------------------------
+//------------------------------------------------------------
+//---------------------------YELP-----------------------------
+//------------------------------------------------------------
 function searchYelpData(request, response){
 	const URL = `https://api.yelp.com/v3/businesses/search?latitude=${request.query.data.latitude}&longitude=${request.query.data.longitude}`;
 	let location_id = request.query.data.id;
 
+	//604800000 = update every week
 	checkDatabase(location_id, response, SQL_CMDS.getRestaurants, SQL_CMDS.deleteRestaurant, 604800000).then(result => {
 		if(result === 'NOT IN DATABASE'){
 			superagent.get(URL).set('Authorization', `Bearer ${process.env.YELP_API_KEY}`).then(result => 	{
 				let yelpResults = JSON.parse(result.text).businesses;
 				let topRestaurants = yelpResults.map(restaurantObj => {
-		
+
 					let name = restaurantObj.name;
 					let image_url = restaurantObj.image_url;
 					let price = restaurantObj.price;
 					let rating = restaurantObj.rating;
-					let url = restaurantObj.url;		
-			
+					let url = restaurantObj.url;
+
 					let responseRestaurantObject = new YelpData(name, image_url, price, rating, url, location_id);
-		
+
 					client.query(SQL_CMDS.insertRestaurants, [responseRestaurantObject.name, responseRestaurantObject.image_url, responseRestaurantObject.price, responseRestaurantObject.rating, responseRestaurantObject.url, responseRestaurantObject.created_at, responseRestaurantObject.location_id]);
-		
+
 					return responseRestaurantObject;
 				});
 				response.send(topRestaurants);
@@ -264,11 +241,7 @@ function searchYelpData(request, response){
 			);
 		}
 	});
-
-
 }
-
-
 
 //---------------------------INITIATE SERVER---------------------------------
 app.listen(PORT, () => {
